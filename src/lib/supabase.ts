0330c7e22d20983e470a0b3c82f7a70dcbca575c
SshauryaaRocks19 <198586011+SshauryaaRocks19@users.noreply.github.com>
2026-08-29 16:02:14 +0530

    chore: remove public prefix from supabase url to avoid vercel warnings

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 // We use the service role key server-side only so we can bypass RLS 
